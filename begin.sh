5299a2fa727886b2421f5c05276376d9711aa920
Victor Hugo <vct.bta@gmail.com>
2018-09-30 14:32:32 -0300

    Copia os arquivos .vimrc, .zshrc e .tmux.conf no script begin.sh

--- begin.sh
+++ begin.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-if [ -e ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.old
-fi
-cp .vimrc ~
+# Copia um arquivo para outro diretório.
+# Se o arquivo já existir, cria um backup.
+# @param $1 Arquivo à copiar
+# @param $2 Diretório destino
+copy_to_home () {
+    if [ -e "$2/$1" ]; then
+        mv "$2/$1" "$2/$1.old"
+    fi
+    cp $1 $2
+}
+
+copy_to_home .vimrc ~
+copy_to_home .zshrc ~
+copy_to_home .tmux.conf ~
 
