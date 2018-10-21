6c8a375536a22581208484fb23e14ef7f3b5fa69
Victor Hugo <vct.bta@gmail.com>
2018-10-21 00:11:16 -0300

    Adiciona arte ascii ao copiador de arquivos

--- begin.sh
+++ begin.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo -e "\e[95m"
+echo '     dBBBBb  dBBBBP dBBBBBBP    dBBBBP dBP dBP    dBBBP.dBBBBP'
+echo '        dBP dBP.BP                                     BP'
+echo '   dBP dBP dBP.BP    dBP      dBBBP  dBP dBP    dBBP   `BBBBb'
+echo '  dBP dBP dBP.BP    dBP      dBP    dBP dBP    dBP        dBP'
+echo -e " dBBBBBP dBBBBP    dBP      dBP    dBP dBBBBP dBBBBP dBBBBP\`\e[0m\e[94m"
+echo '  __   ____ ______ ______  ____ ____'
+echo ' (( \ ||    | || | | || | ||    || \\'
+echo '  \\  ||==    ||     ||   ||==  ||_//'
+echo ' \_)) ||___   ||     ||   ||___ || \\'
+echo -e "\e[0m"
+
 # Copia um arquivo para outro diretório.
 # Se o arquivo já existir, cria um backup.
 # @param $1 Arquivo à copiar
@@ -21,3 +33,4 @@ cd .config/nvim/
 copy_to_home init.vim ~/.config/nvim
 cd - > /dev/null
 
+exit 0
